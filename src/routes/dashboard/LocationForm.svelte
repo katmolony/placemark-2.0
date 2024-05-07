4b106984223a5b59cab58b7edcab3f06da2d7711
Kate Molony <katemolony96@gmail.com>
2024-05-07 17:04:27 +0100

    Add location form

--- src/routes/dashboard/LocationForm.svelte
+++ src/routes/dashboard/LocationForm.svelte
@@ -10,16 +10,24 @@
 //     type: Schema.Types.ObjectId,
 //     ref: "User",
 //   },
+
+let title = "Galway";
+let imageURL = "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTwn4EZh1ZftOnaus7nT7Qoujoyn00W7smzMBxvz7kcVBYHfj8R8oLtQAD1NAJZSMvPb89oXO-VY9Vl3jpjwV1g4KaVd54OfHzGw38fUPM";
+
+async function addLocation() {
+    console.log(`Just added: ${title} with image ${imageURL}`);
+  }
+
 </script>
 
-<form>
+<form on:submit|preventDefault={addLocation}>
   <div class="field">
     <label class="label" for="title">Enter City Name:</label>
-    <input class="input" id="title" name="amount" type="string" />
+    <input class="input" id="title" name="title" type="string" />
   </div>
   <div class="field">
-    <label class="label" for="title">Enter Image URL:</label>
-    <input class="input" id="imageURL" name="amount" type="string" />
+    <label class="label" for="imageURL">Enter Image URL:</label>
+    <input class="input" id="imageURL" name="imageURL" type="string" />
   </div>
 
   <div class="field">
