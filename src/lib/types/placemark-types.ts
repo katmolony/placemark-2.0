2a2e4f806ac1d5b8d43b230a10d38e17212982d5
Kate Molony <katemolony96@gmail.com>
2024-05-09 16:53:32 +0100

    Add model types

--- src/lib/types/placemark-types.ts
+++ src/lib/types/placemark-types.ts
@@ -1,14 +1,48 @@
 export interface Session {
-    name: string;
-    _id: string;
-    token: string;
-  }
-  
+  name: string;
+  _id: string;
+  token: string;
+}
+
 export interface User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    userType: string;
-    _id?: string;
-  };
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  userType: string;
+  _id?: string;
+}
+
+export interface Location {
+  title: string;
+  imageURL: string;
+  lat: number;
+  lng: number;
+  temp: number;
+  weather: string;
+  userid: User | string; // how to get this
+}
+
+export type BusinessCategories = "Accommodation" | "Dining" | "Shopping" | "Nightlife" | "Activities";
+
+export interface Business {
+  title: string;
+  address: string;
+  description: string;
+  lat: number;
+  lng: number;
+  category: BusinessCategories[];
+  locationid: Location | string;
+  reviews: Review[];
+  _id: string;
+}
+
+export interface Review extends Document {
+  content: string;
+  rating: number;
+  businessid: Business| string;
+  userid: User| string;
+  createdAt: Date;
+  updatedAt: Date;
+  timestamp: string;
+}
