8b79428cdcb6d4f1d0deeee619f146c8a3fa92b6
Kate Molony <katemolony96@gmail.com>
2024-05-09 17:09:21 +0100

    Add service functions

--- src/lib/services/placemark-service.ts
+++ src/lib/services/placemark-service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { Session, User } from "$lib/types/placemark-types";
+import type { Location, Business, Review } from "$lib/types/placemark-types";
 
 export const placemarkService = {
   baseUrl: "http://kates-macbook-air-2.local:4000",
@@ -32,5 +33,35 @@ export const placemarkService = {
       console.log(error);
       return null;
     }
-  }
+  },
+
+  async addBusiness(business: Business, session: Session) {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + session.token;
+      const response = await axios.post(this.baseUrl + "/api/locations/" + business.locationid + "/businesss", business);
+      return response.status == 200;
+    } catch (error) {
+      return false;
+    }
+  },
+
+  async getLocations(session: Session): Promise<Location[]> {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + session.token;
+      const response = await axios.get(this.baseUrl + "/api/locations");
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  },
+
+  async getBusinesss(session: Session): Promise<Business[]> {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + session.token;
+      const response = await axios.get(this.baseUrl + "/api/businesss");
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  }  
 };
