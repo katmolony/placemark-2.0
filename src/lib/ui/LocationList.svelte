c8d74f8b3d7ce56c6e11450df583eb50f8d7c61b
Kate Molony <katemolony96@gmail.com>
2024-05-10 19:27:00 +0100

    Edit columns of location list

--- src/lib/ui/LocationList.svelte
+++ src/lib/ui/LocationList.svelte
@@ -2,32 +2,6 @@
   import type { Location } from "$lib/types/placemark-types";
 
 export let locations: Location[];
-  // export let locations = [
-  //   {
-  //     title: "Greece",
-  //     imageURL: "https://cdn.britannica.com/82/195482-050-2373E635/Amalfi-Italy.jpg",
-  //     lat: 100,
-  //     lng: 200,
-  //     weather: "Sunny",
-  //     temp: 23.5
-  //   },
-  //   {
-  //     title: "Italy",
-  //     imageURL: "https://cdn.britannica.com/82/195482-050-2373E635/Amalfi-Italy.jpg",
-  //     lat: 100,
-  //     lng: 200,
-  //     weather: "Sunny",
-  //     temp: 23.5
-  //   },
-  //   {
-  //     title: "Spain",
-  //     imageURL: "https://lh5.googleusercontent.com/p/AF1QipPwWQvGO_HOnmjoMvNAeLcyrAK5uPjTlNFvxTws=w540-h312-n-k-no",
-  //     lat: 100,
-  //     lng: 200,
-  //     weather: "Sunny",
-  //     temp: 23.5
-  //   }
-  // ];
 </script>
 
 <table class="table is-fullwidth">
