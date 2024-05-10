72edb5e1350d96dc7ab764bb1758578d3d5abd6c
Kate Molony <katemolony96@gmail.com>
2024-05-10 20:04:21 +0100

    Add latest loaction store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { writable } from "svelte/store";
-import type { Session } from "$lib/types/placemark-types";
+import type { Location, Session } from "$lib/types/placemark-types";
 
 export const currentSession = writable<Session>(); // email of the currently logged in user
 export const subTitle = writable<string>(); // sub title to be used on a window
+export const latestLocation = writable<Location>();
