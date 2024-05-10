52bdd3fcefb6bc69a407c9e725842601620fc0d4
Kate Molony <katemolony96@gmail.com>
2024-05-10 14:49:01 +0100

    Get and list locations

--- src/routes/favourite/+page.svelte
+++ src/routes/favourite/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  import { subTitle } from "$lib/stores";
+  import { placemarkService } from "$lib/services/placemark-service";
+  import type { Location } from "$lib/types/placemark-types";
+  import { currentSession, subTitle } from "$lib/stores";
   import Card from "$lib/ui/Card.svelte";
   import LocationList from "$lib/ui/LocationList.svelte";
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
 
   subTitle.set("Favourite Locations");
+  let locations: Location[] = [];
+  onMount(async () => {
+    locations = await placemarkService.getLocations(get(currentSession));
+  });
 </script>
 
 <Card title="Favourite Locations">
-  <LocationList />
+  <LocationList {locations}/>
 </Card>
