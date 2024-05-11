cfdaf2df4c4be08ac89a38979c5c12928297d21e
Kate Molony <katemolony96@gmail.com>
2024-05-11 13:38:32 +0100

    Edit out code

--- src/routes/charts/+page.svelte
+++ src/routes/charts/+page.svelte
@@ -9,15 +9,6 @@
 
   subTitle.set("Welcome to the Charts");
 
-  // const chartData = {
-  //   labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
-  //   datasets: [
-  //     {
-  //       values: [10, 12, 3, 9, 8, 15, 9]
-  //     }
-  //   ]
-  // };
-
   const topTemps = {
     labels: ["", "", ""],
     datasets: [
