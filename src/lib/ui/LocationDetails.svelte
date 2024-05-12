11241c6cac38c766e1a807c1951a785550cf29a2
Kate Molony <katemolony96@gmail.com>
2024-05-12 15:50:32 +0100

    Add location details page

---
+++ src/lib/ui/LocationDetails.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Location } from "$lib/types/placemark-types";
+
+  export let location: Location | null = null;
+</script>
+
+{#if location}
+  <div class="location-container">
+    <div class="location-details">
+      <p>Latitude: {location.lat}, Longitude: {location.lng}</p>
+      <p>Weather: {location.weather}</p>
+      <p>Temperature: {location.temp}°C</p>
+    </div>
+  </div>
+{:else}
+  <p>Loading location details...</p>
+{/if}
