8dbc8dcecc8913134c7dc1bdba8510d7e11e14de
Kate Molony <katemolony96@gmail.com>
2024-05-16 14:02:58 +0100

    Future features (still needs work)

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,11 +1,12 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { writable } from "svelte/store";
-import type { Business, Location, Session } from "$lib/types/placemark-types";
+import type { Business, Location, Session, Image } from "$lib/types/placemark-types";
 
 export const currentSession = writable<Session>(); // email of the currently logged in user
 export const subTitle = writable<string>(); // sub title to be used on a window
 export const latestLocation = writable<Location>();
 export const latestBusiness = writable<Business>();
+export const latestImage = writable<Image>();
 
 export const currentLocation = writable<Location>();
 export const currentLocationId = writable<string>();
