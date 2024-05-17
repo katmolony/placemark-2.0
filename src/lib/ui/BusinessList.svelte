b7e209fe333e0c8faf94e21fd1a2060fdad06d1c
Kate Molony <katemolony96@gmail.com>
2024-05-17 10:17:05 +0100

    Loop through weather categories

--- src/lib/ui/BusinessList.svelte
+++ src/lib/ui/BusinessList.svelte
@@ -1,40 +1,19 @@
 <script lang="ts">
-    import type { Business } from "$lib/types/placemark-types";
-  
-    export let businesses: Business[];
-  </script>
-  
-  <table class="table is-fullwidth">
-    <thead>
-      <th>Business Name</th>
-      <th>Category</th>
-      <th>Coordinates</th>
-      <th>Address</th>
-      <th>description</th>
-      <th></th>
-    </thead>
-    <tbody>
-      {#each businesses as business}
-        <tr>
-          <td>
-            {business.title}
-          </td>
-          <td>
-            {business.category}
-          </td>
-          <td>
-            {business.lat}, {business.lng}
-          </td>
-          <td>
-            {business.address}
-          </td>
-          <td>
-            {business.description}
-          </td>
-          <td>
-            <a class="button" href="/business"><i class="fas fa-search-business"></i></a>
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+  import type { Business } from "$lib/types/placemark-types";
+  import Card from "./Card.svelte";
+
+  export let businesses: Business[];
+</script>
+
+{#each businesses as business}
+  <Card title="">
+    <h3>{business.title}</h3>
+    <p><strong>Category:</strong> {business.category}</p>
+    <!-- <p><strong>Coordinates:</strong> {business.lat}, {business.lng}</p> -->
+    <p><strong>Address:</strong> {business.address}</p>
+    <p><strong>Description:</strong> {business.description}</p>
+    <a href="/business" class="button">
+      <!-- <FontAwesomeIcon icon={faSearch} /> -->
+    </a>
+  </Card>
+{/each}
